32a5638461c70012ee11b1dfc39fbf3111db50b5
Jason Mavandi <mvndaai@gmail.com>
2026-04-23 13:44:25 -0600

    improve metronome

--- lib/services/tone_player.dart
+++ lib/services/tone_player.dart
@@ -9,6 +9,10 @@ class TonePlayer {
   Timer? _metronomeTimer;
   bool _isMetronomeRunning = false;
   final _platformPlayer = platform.createPlatformPlayer();
+  
+  // Track the next expected beat time for more accurate scheduling
+  DateTime? _nextBeatTime;
+  double _currentBpm = 120;
 
   bool get isMetronomeRunning => _isMetronomeRunning;
 
@@ -22,6 +26,7 @@ class TonePlayer {
   void startMetronome(double bpm, {String sound = 'tick', void Function()? onBeat}) {
     stopMetronome();
 
+    _currentBpm = bpm;
     final intervalMs = (60000.0 / bpm).round();
     _isMetronomeRunning = true;
 
@@ -29,13 +34,37 @@ class TonePlayer {
     _playMetronomeClick(sound);
     onBeat?.call();
 
-    _metronomeTimer = Timer.periodic(
-      Duration(milliseconds: intervalMs),
-      (_) {
-        _playMetronomeClick(sound);
-        onBeat?.call();
-      },
-    );
+    _nextBeatTime = DateTime.now().add(Duration(milliseconds: intervalMs));
+    _scheduleNextBeat(sound, onBeat);
+  }
+
+  void _scheduleNextBeat(String sound, void Function()? onBeat) {
+    if (!_isMetronomeRunning || _nextBeatTime == null) return;
+
+    final now = DateTime.now();
+    final delay = _nextBeatTime!.difference(now);
+    
+    // If we're late, play immediately and catch up
+    if (delay.inMilliseconds <= 0) {
+      _tick(sound, onBeat);
+      return;
+    }
+
+    _metronomeTimer = Timer(delay, () => _tick(sound, onBeat));
+  }
+
+  void _tick(String sound, void Function()? onBeat) {
+    if (!_isMetronomeRunning) return;
+
+    _playMetronomeClick(sound);
+    onBeat?.call();
+
+    // Calculate next beat time based on the previous expected time, not the actual time
+    // This prevents drift from accumulating.
+    final intervalMs = (60000.0 / _currentBpm).round();
+    _nextBeatTime = _nextBeatTime!.add(Duration(milliseconds: intervalMs));
+    
+    _scheduleNextBeat(sound, onBeat);
   }
 
   /// Stops the metronome.
@@ -43,6 +72,7 @@ class TonePlayer {
     _metronomeTimer?.cancel();
     _metronomeTimer = null;
     _isMetronomeRunning = false;
+    _nextBeatTime = null;
   }
 
   /// Plays a metronome click sound.
