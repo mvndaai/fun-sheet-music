743fcd4063dcc97b7c62600c96d38a523a492396
mvndaai <mvndaai@gmail.com>
2026-05-13 22:21:53 +0000

    Deploying to gh-pages from @ mvndaai/fun-sheet-music@0308e1f04446fe228e899e340ddbbc8115728090 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,7 +38,7 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1784027702" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "1291869107" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
   onEntrypointLoaded: async function(engineInitializer) {
     const engine = await engineInitializer.initializeEngine();
